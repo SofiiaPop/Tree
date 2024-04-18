ae817bd6c62b7adf5b9c2173b3580e6ba5522950
Sofiia Popeniuk <146963804+SofiiaPop@users.noreply.github.com>
2024-04-18 22:23:09 +0300

    Update sort_binary_tree_by_levels.py
    Using deque

--- SortBinaryTreeByLevels/sort_binary_tree_by_levels.py
+++ SortBinaryTreeByLevels/sort_binary_tree_by_levels.py
@@ -1,13 +1,18 @@
+from collections import deque
+
 def tree_by_levels(node):
     if node is None:
         return []
-    stack = [node]
-    output = []
+    stack = deque([node])
+    output = deque([])
     while stack:
-        current = stack.pop(0)
+        current = stack.popleft()
         output.append(current.value)
         if current.left:
             stack.append(current.left)
         if current.right:
             stack.append(current.right)
-    return output
+    result = []
+    for el in output:
+        result.append(el)
+    return result
